5e4c1a984f9c6322a60313dd7ba3055125cba60e
Daniel Marquard <daniel@netidy.com>
2017-01-06 06:32:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+<a href="https://critcola.com/?utm_source=github.com&utm_medium=readme&utm_term=logo&utm_content=discord-auto-grouping&utm_campaign=development">![Logo](https://critcola.com/assets/images/crit-cola-banner.svg)</a>
+
+# Discord Auto-grouping
+
+Discord Auto-grouping is a Discord bot that uses discord.js to automatically group users into "subchannels" in Discord servers, eliminating the need for creating numerous group channels. It was developed out of necessity for Crit Cola to mitigate two major pitfalls with Discord, their lack of subchannels and temporary channels. When a user enters a channel prefixed with a game controller emoji (🎮), they'll be automatically grouped into their own "subchannel" where they can be joined by the rest of their group. When the group channel empties, it will be immediately deleted. Crit Cola is perhaps the first community on Discord to achieve auto-grouping, and we're open-sourcing the code so that other communities can benefit from it.
+
+## Demo
+
+See it in action and test it out for yourself in [Crit Cola's Discord](https://discord.critcola.com).
+
+## Installation
+
+Install [discord.js](https://github.com/hydrabolt/discord.js), ignoring dependency warnings, then run `node discord-auto-grouping.js` after setting your token in `client.login()`.
+
+```sh
+npm install discord.js --save
+npm install uws --save
+```
+
+## About Crit Cola
+
+Crit Cola is connecting and empowering the world's best players. Primarily an [Overwatch clan](https://critcola.com/?utm_source=github.com&utm_medium=readme&utm_term=overwatch-clan&utm_content=discourse-browser-share&utm_campaign=development), we're a growing community of PC gamers. Join our [Steam group](http://steamcommunity.com/groups/critcola) and follow us on [Twitter](https://twitter.com/CritColaGaming). Cheers!
+
+## License
+
+Discourse Auto-grouping is released under the [MIT License](LICENSE).
---
+++ discord-auto-grouping.js
@@ -0,0 +1,73 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// Connect and perform routine maintenance.
+client.on('ready', () => {
+	console.log('[' + new Date().toISOString() + '] Connected!');
+	
+	// Get a list of channels.
+	var channelsOrdered = client.channels.array().slice(0);
+	
+	// Evaluate only voice channels.
+	channelsOrdered = channelsOrdered.filter(function(channel) {
+		return channel.type == 'voice' && channel.position !== 'undefined';
+	});
+	
+	// Sort channels by their current position.
+	channelsOrdered = channelsOrdered.sort(function(channelA, channelB) {
+		return channelA.position - channelB.position;
+	});
+	
+	// Re-sort channels to support auto-grouping and maximum voice quality.
+	var currentPosition = 100;
+	channelsOrdered.forEach(function(channel) {
+		currentChannel = client.channels.get(channel.id);
+		currentChannel.edit({bitrate: 96000, position: currentPosition})
+			.then(editedChannel => {
+				console.log('[' + new Date().toISOString() + '] Set ' + editedChannel.type + ' channel "' + editedChannel.name + '" (' + editedChannel.id + ') position to ' + editedChannel.position + ' with ' + editedChannel.bitrate / 1000 + 'kbps bitrate')
+			})
+			.catch(console.error);
+		currentPosition += 100;
+	});
+});
+
+
+// Trigger on VOICE_STATE_UPDATE events.
+client.on('voiceStateUpdate', (oldMember, member) => {
+	
+	// Check if the user entered a new channel.
+	if (member.voiceChannelID) {
+		const newChannel = member.guild.channels.get(member.voiceChannelID);
+		
+		// If the user entered a game channel (prefixed with a game controller icon), group them into their own channel.
+		if (newChannel.name.startsWith('🎮')) {
+			member.guild.createChannel('Group', 'voice')
+				.then(createdChannel => {
+					createdChannel.edit({bitrate: 96000, position: newChannel.position + 1})
+						.then(createdChannel => {
+							member.setVoiceChannel(createdChannel)
+								.then(console.log('[' + new Date().toISOString() + '] Moved user "' + member.user.username + '#' + member.user.discriminator + '" (' + member.user.id + ') to ' + createdChannel.type + ' channel "' + createdChannel.name + '" (' + createdChannel.id + ')'))
+								.catch(console.error);
+						})
+						.catch(console.error);
+				})
+				.catch(console.error);
+		}
+	}
+	
+	// Check if the user came from another channel.
+	if (oldMember.voiceChannelID) {
+		const oldChannel = oldMember.guild.channels.get(oldMember.voiceChannelID);
+		
+		// Delete the user's now empty temporary channel, if applicable.
+		if (oldChannel.name.includes('Group') && !oldChannel.members.array().length) {
+			oldChannel.delete()
+				.then(function() {
+					console.log('[' + new Date().toISOString() + '] Deleted ' + oldChannel.type + ' channel "' + oldChannel.name + '" (' + oldChannel.id + ')');
+				})
+				.catch(console.error);
+		}
+	}
+});
+
+client.login('');
